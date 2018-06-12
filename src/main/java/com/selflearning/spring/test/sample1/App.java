1bb90e3fd811d918109a0b5969a3a658a97e837a
Sean Cui <sean900531@hotmail.com>
2018-06-12 10:09:25 -0400

    more tutorials

--- src/main/java/com/selflearning/spring/test/sample1/App.java
+++ src/main/java/com/selflearning/spring/test/sample1/App.java
@@ -8,14 +8,15 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	ApplicationContext context = new ClassPathXmlApplicationContext("com/selflearning/spring/test/sample1/beans/beans.xml");
-    	
-    	Person p = (Person)context.getBean("person");
-    	p.speak();
-    	((ClassPathXmlApplicationContext)context).close();
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/selflearning/spring/test/sample1/beans/beans.xml");
+
+		Person p = (Person) context.getBean("person");
+		p.speak();
+
+		System.out.println(p);
+		((ClassPathXmlApplicationContext)context).close();
+	}
 }
--- src/main/java/com/selflearning/spring/test/sample1/Person.java
+++ src/main/java/com/selflearning/spring/test/sample1/Person.java
@@ -1,7 +1,49 @@
 package com.selflearning.spring.test.sample1;
 
 public class Person {
+	private int id;
+	private String name;
+
+	private int taxId;
+
+	public Person() {
+
+	}
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 	public void speak() {
 		System.out.println("Hello!");
 	}
+
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId + "]";
+	}
+
+	
+
 }
